c266113bd64b71bb3466bfbfcddbd2bb0350cf44
Marshall McDonnell <mcdonnellmt@ornl.gov>
2018-07-05 16:54:21 -0400

    Working, refactored widom

--- src/widom.cpp
+++ src/widom.cpp
@@ -20,7 +20,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <limits>
+#include <vector>
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -31,12 +33,23 @@
 #include "lammps.h"         // these are LAMMPS include files
 #include "domain.h"
 #include "input.h"
+#include "compute.h"
+#include "modify.h"
+#include "neighbor.h"
 #include "atom.h"
 #include "force.h"
 #include "pair.h"
-#include "pair_lj_cut.h"
+#include "bond.h"
+#include "angle.h"
+#include "dihedral.h"
+#include "improper.h"
+#include "kspace.h"
 #include "library.h"
 #include "math_const.h"
+#include "comm.h"
+#include "update.h"
+#include "read_dump.h"
+#include "error.h"
 
 #include "geometry.h"
 #include "output_widom.h"
@@ -47,7 +60,83 @@ double fRand(double fMin, double fMax)
     return fMin + f * (fMax - fMin);
 };
 
+int count_words( std::string s ) {
+    int word_count(0);
+    std::stringstream ss(s);
+    std::string word;
+    while( ss >> word ) ++word_count;
+    return word_count;
+}
+
+std::vector<char*> vector_str2cstr(std::vector<std::string> strings) {
+    std::vector<char*> cstrings;
+    cstrings.reserve(strings.size());
+
+    for(size_t i = 0; i < strings.size(); ++i)
+        cstrings.push_back(const_cast<char*>(strings[i].c_str()));
+    return cstrings;
+}
+
 using namespace LAMMPS_NS;
+
+double energy_full(void *ptr)
+{
+  LAMMPS *lmp = (LAMMPS *) ptr;
+  int imolecule;
+
+  if (lmp->domain->triclinic) lmp->domain->x2lamda(lmp->atom->nlocal);
+  lmp->domain->pbc();
+  lmp->comm->exchange();
+  lmp->atom->nghost = 0;
+  lmp->comm->borders();
+  if (lmp->domain->triclinic) lmp->domain->lamda2x(lmp->atom->nlocal+lmp->atom->nghost);
+  if (lmp->modify->n_pre_neighbor) lmp->modify->pre_neighbor();
+  lmp->neighbor->build();
+  int eflag = 1;
+  int vflag = 0;
+  // clear forces so they don't accumulate over multiple
+  // calls within fix gcmc timestep, e.g. for fix shake
+
+  size_t nbytes = sizeof(double) * (lmp->atom->nlocal + lmp->atom->nghost);
+  if (nbytes) memset(&lmp->atom->f[0][0],0,3*nbytes);
+
+  if (lmp->modify->n_pre_force) lmp->modify->pre_force(vflag);
+
+  if (lmp->force->pair) lmp->force->pair->compute(eflag,vflag);
+
+  if (lmp->atom->molecular) {
+    if (lmp->force->bond) lmp->force->bond->compute(eflag,vflag);
+    if (lmp->force->angle) lmp->force->angle->compute(eflag,vflag);
+    if (lmp->force->dihedral) lmp->force->dihedral->compute(eflag,vflag);
+    if (lmp->force->improper) lmp->force->improper->compute(eflag,vflag);
+  }
+
+  if (lmp->force->kspace) lmp->force->kspace->compute(eflag,vflag);
+
+  // unlike Verlet, not performing a reverse_comm() or forces here
+  // b/c GCMC does not care about forces
+  // don't think it will mess up energy due to any post_force() fixes
+
+  if (lmp->modify->n_post_force) lmp->modify->post_force(vflag);
+  if (lmp->modify->n_end_of_step) lmp->modify->end_of_step();
+
+  // NOTE: all fixes with THERMO_ENERGY mask set and which
+  //   operate at pre_force() or post_force() or end_of_step()
+  //   and which user has enable via fix_modify thermo yes,
+  //   will contribute to total MC energy via pe->compute_scalar()
+
+  // Setup pe compute (c_pe)
+  class Compute *c_pe;
+  char *id_pe = (char *) "thermo_pe";
+  int ipe = lmp->modify->find_compute(id_pe);
+  c_pe = lmp->modify->compute[ipe];
+
+  lmp->update->eflag_global = lmp->update->ntimestep;
+  double total_energy = c_pe->compute_scalar();
+
+  return total_energy;
+}
+
 
 int main(int narg, char **arg)
 {
@@ -146,19 +235,42 @@ int main(int narg, char **arg)
     y = fRand( ylo, yhi );
     z = fRand( zlo, zhi );
 
-    // Add one atom
+    // Get initial number of atoms
+    int natoms_old = static_cast<int> (lmp->atom->natoms);
+
+    // Setup add atom command
     char add_command[216];
-    sprintf(add_command, "create_atoms 1 single %f %f %f", x, y, z);
+    sprintf(add_command, "create_atoms 1 single %f %f %f units box", x, y, z);
+
+    // Setup delete atom command
+    char delete_command[216];
+    sprintf(delete_command, "delete_atoms group test_particle");
+
+    // Add atom
     lmp->input->one(add_command);
-    int natoms = static_cast<int> (lmp->atom->natoms);
-    int test_particle = natoms - 1;
+    int natoms_new = static_cast<int> (lmp->atom->natoms);
 
-    
-    // Use read-in xyz for atoms for 1st step
-    int *type = new int[natoms];
-    double *r = new double[3*natoms];
+    // Get original N system group and test particle N+1 system group
+    char group_original_command[216];
+    sprintf(group_original_command, "group original_system id %d:%d", 1, natoms_old);
+    lmp->input->one(group_original_command);
+    char group_test_particle_command[216];
+    sprintf(group_test_particle_command, "group test_particle id %d:%d", natoms_old+1, natoms_new);
+    lmp->input->one(group_test_particle_command);  
 
+    // Delete atom to reset before loop
+    lmp->input->one(delete_command); 
 
+    double energy;
+    energy = energy_full(lmp);
+    printf("ENERGY: %f\n", energy);
+
+    // Gather xyz and check gather + scatter
+    double *r = new double[3*natoms_old];
+    lammps_gather_atoms(lmp,"x",1,3,r);
+    lammps_scatter_atoms(lmp,"x",1,3,r);
+
+    // Open trajectory file 
     std::ifstream file;
     file.open( "dump.all.lammpstrj" );
     if( !file.is_open() ) {
@@ -166,6 +278,7 @@ int main(int narg, char **arg)
       exit(1);
     }
 
+    // Pick either XYZ or LAMMPS default atom-style
     bool xyz = false;
     bool lammps = true;
     if( xyz && lammps ) {
@@ -173,20 +286,23 @@ int main(int narg, char **arg)
       exit(1);
     }
     
+    // Initialize variables
     std::string element;
     int id, atype;
     double rsq, f, eij;
     double wtest, wtest_sq;
     double expE_kbT;
     double beta_mu, stdev_beta_mu;
+    int nsamples = 0;
 
+
+    // Setup beta term
     double T = 1.0;
     double kb; 
     kb = static_cast<double> (lmp->force->boltz);
     double beta = 1.0 / ( kb * T );
 
-    int nsamples = 0;
-
+    // Setup output file
     std::ofstream outfile( "out.file" );
     OutputWidom output;
     if (me == 0) {
@@ -194,119 +310,82 @@ int main(int narg, char **arg)
       output.header(std::cout);
     }
 
-    // Gather types
-    type = ( (int*) lammps_extract_atom(lmp,"type") );
-
-    // Add tail corrections to pair style (Lennard-Jones only)
-    bool tail_flag = true;
-    double *etail_ij;
-    double V, rho, etail;
-    double eps = 1.0;
-    double sig = 1.0;
-    double rc = 3.5;
-    double irc = 1.0 / rc;
-    double irc3 = irc*irc*irc;
-    double irc6 = irc3*irc3;
-    double irc9 = irc6*irc3;
-
-    etail_ij = new double[natoms];     
-    if( tail_flag ) {
-
-
-      for( int i = 0; i < natoms; i++ ) {
-
-        // Get the number of each particle type
-        double count[2];
-        count[0] = count[1] = 0.0;
-        for (int k = 0; k < natoms; k++) {
-          if (type[k] == type[i])        count[0] += 1.0; //# of particle type i
-          if (type[k] == type[test_particle]) count[1] += 1.0; //# of inserted particle type
-        }
-       
-        double tempcut; 
-        tempcut = lmp->force->pair->init_one( type[i], type[test_particle] );
-        etail_ij[i]  = lmp->force->pair->etail_ij;
-        etail_ij[i] /= count[1];
-        V = (lmp->domain->xprd * lmp->domain->yprd * lmp->domain->zprd);
-        rho = (natoms - 1) / V; //Subtract test particle in rho
-        etail_ij[i] /= V;
-        etail = (8.0/3.0)*M_PI*rho*eps*pow(sig, 3.0);
-        etail *= ( (1.0/3.0)*irc9 - irc3 );
-
-      }
-    }
-
     // initialize random seed
     srand(3987);
 
+    // Setup reader
+    ReadDump *rd = new ReadDump(lmp);
+    int timestep = 0;
+    std::vector<std::string> read_dump_string {"dump.all.lammpstrj", "0", 
+                                             "x", "y", "z", 
+                                             "scaled", "yes",
+                                             "box", "yes",
+                                             "replace", "yes",
+                                             "format", "native"};
+    char **read_dump_args = (char**)calloc(13, sizeof(char*));
+    for (int i = 0; i < read_dump_string.size(); i++ )
+        read_dump_args[i] = (char *)read_dump_string[i].c_str();
+
+    rd->store_files(1,&read_dump_args[0]);
+    int narg = read_dump_string.size();
+    int nremain = narg - 2;
+    if (nremain)
+      nremain = rd->fields_and_keywords(nremain,&read_dump_args[narg-nremain]);
+    else nremain = rd->fields_and_keywords(0,NULL);
+    if (nremain) rd->setup_reader(nremain,&read_dump_args[narg-nremain]);
+    else rd->setup_reader(0,NULL);
+
+    // Turn off screen
+    lmp->screen = NULL;
+
     // Loop over snapshots of trajectory file
-    while( !file.eof() ) {
+    int nstep = 0;
+    std::string line;
+    while( 1 ) {
 
-      // gather atom coordinates for this timestep
-      lammps_gather_atoms(lmp,"x",1,3,r);
-
-      // XYZ file
-      if( xyz ) {
-
-        // skip 1st 2 lines of XYZ
-        for( int i = 0; i < 2; i++ )
-          file.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-
-        // read in xyz 
-        for( int i = 0; i < (natoms-1); i++ )  
-          file >> element >> r[3*i + 0] >> r[3*i + 1] >> r[3*i + 2];
+      // Read in nextsnapshot
+      bigint ntimestep = rd->seek(nstep,1);
+      if (ntimestep < 0)
+        break;
+      rd->header(1);
+      lmp->update->reset_timestep(nstep);
+      rd->atoms();
       
-        file.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-      }
-
-      if( lammps ) {
-        // skip 1st 9 lines of XYZ
-        for( int i = 0; i < 9; i++ )
-          file.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-
-        // read in xyz 
-        for( int i = 0; i < (natoms-1); i++ )  
-          file >> id >> atype >> r[3*i + 0] >> r[3*i + 1] >> r[3*i + 2];
-      
-        file.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-      }
+      // get initial pe for N system to get diff from N+1 system
+      double initial_energy;
+      initial_energy = energy_full(lmp);
 
       // initialize energy sum for insertion
       int ninserts = 10000;
       wtest    = 0.0;
       wtest_sq = 0.0;
-
+      
       // Peform test particle insertion loop
-      double cut   = lmp->force->pair->cutforce;
-      double cutsq = cut * cut;
+      double new_energy, delta_energy;
       for( int insert = 0; insert < ninserts; insert++ ) {
 
-        // use random coordinates for insertion atom 
-        r[3*(test_particle) + 0] = fRand( xlo, xhi );
-        r[3*(test_particle) + 1] = fRand( ylo, yhi );
-        r[3*(test_particle) + 2] = fRand( zlo, zhi );
+        // get random point for COM of test particle
+        x = fRand(xlo, xhi);
+        y = fRand(ylo, yhi);
+        z = fRand(zlo, zhi);
 
-        // scatter atoms so we can compute energy w/ new coordinates       
-        lammps_scatter_atoms(lmp,"x",1,3,r);
+        // Add test particle
+        sprintf(add_command, "create_atoms 1 single %f %f %f units box", x, y, z);
+        lmp->input->one(add_command);
 
-        // Compute PE for insertion atom
-        double energy = 0.0;
-        for( int i = 0; i < natoms-1; i++ ) {
-          rsq     = getDistanceSq( i, test_particle, r, bbox );
-        
-          if( rsq <= cutsq ) {
-            eij = lmp->force->pair->single( i, test_particle, type[i], type[test_particle], rsq, 1.0, 1.0, f);
-            energy += eij;
-          }
-        }
+        // Get energy diff from N -> N+1 system
+        new_energy = energy_full(lmp);
+        delta_energy = new_energy - initial_energy;
 
         // collect energy sum for insertion
-        expE_kbT  = exp( -1.0 * beta * energy);
+        expE_kbT  = exp( -1.0 * beta * delta_energy);
         wtest    += expE_kbT;
         wtest_sq += expE_kbT * expE_kbT;
 
-        // collect atoms back up to re-insert last atom
-        lammps_gather_atoms(lmp,"x",1,3,r);
+        // Delete one atom again
+        lmp->input->one(group_test_particle_command);   
+        lmp->input->one(delete_command);
+
       }
 
       // collect chemical potential & stats
@@ -314,26 +393,21 @@ int main(int narg, char **arg)
       stdev_beta_mu = ( wtest_sq - wtest )  / (double) ninserts;
       stdev_beta_mu = sqrt( stdev_beta_mu ) / wtest;
 
-      if( tail_flag )
-        beta_mu += 2.0*etail;
-        //beta_mu += 2.0*etail_ij[test_particle];
-
-      // scatter atoms back out so we can gather them back up in the loop
-      lammps_scatter_atoms(lmp,"x",1,3,r);
-
       nsamples++;
 
       if (me == 0) {
         output.add_line( outfile, nsamples, beta, beta_mu, stdev_beta_mu );
         output.add_line( std::cout, nsamples, beta, beta_mu, stdev_beta_mu );
       }
+
+      // Increment nstep 
+      nstep++;
     }
 
 
     delete [] r;
     delete [] bbox;
-    if( tail_flag )
-      delete [] etail_ij;
+    //if( tail_flag )
     file.close();
 
   }
